638933cc9a2f16c0c83afa0725dfccfd27121994
Nekozilla <krisagriza@gmail.com>
2021-04-29 20:55:27 +0300

    fixed move file

--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -11,23 +11,13 @@ public class Move : SwitchCharachters
     private Collider2D groundCollider;
     private float dirX;
     private float moveSpeed = 10f;
-    private float jumpForce = 100f;
-    
-    private static readonly string ANIMATION_H_SPEED = "hSpeed";
-    private static readonly string ANIMATION_V_SPEED = "vSpeed";
-    private static readonly string ANIMATION_EX_SPEED = "exSpeed";
-    private static readonly string ANIMATION_JUMP = "jump";
-    private static readonly string ANIMATION_GROUNDED = "grounded";
-    private static readonly string ANIMATION_DASHING = "dashing";
-    private static readonly string ANIMATION_WALL = "onWall";
-    private static readonly string ANIMATION_FACING = "facingRight";
-    private static readonly string ANIMATION_LADDER = "onLadder";
-    private static readonly string ANIMATION_INVULNERABLE = "invulnerable";
+    private float jumpForce = 700f;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -38,7 +28,7 @@ public class Move : SwitchCharachters
             DoJump();
 
         playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
-        
+
 
     }
 
@@ -49,31 +39,28 @@ public class Move : SwitchCharachters
             rb.gravityScale = 5;
             moveSpeed = 10f;
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-            jumpForce = 50f;
         }
         else if (avatar2.activeSelf)
         {
             rb.gravityScale = 5;
-            moveSpeed = 50f;
+            moveSpeed = 20f;
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-            jumpForce = 0f;
         }
         else
         {
             rb.gravityScale = 2;
-            moveSpeed = 2f;
+            moveSpeed = 10f;
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-            jumpForce = 100f;
         }
     }
 
-    private void DoJump()
+    public void DoJump()
     {
+
+
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
         }
     }
 
 }
-
-
