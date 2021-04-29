f9e7e0eee46204921c927c8ecdc89d67995e66ca
Nekozilla <krisagriza@gmail.com>
2021-04-29 20:42:45 +0300

    fixed collision broke player movement

--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -11,13 +11,23 @@ public class Move : SwitchCharachters
     private Collider2D groundCollider;
     private float dirX;
     private float moveSpeed = 10f;
-    private float jumpForce = 700f;
+    private float jumpForce = 100f;
+    
+    private static readonly string ANIMATION_H_SPEED = "hSpeed";
+    private static readonly string ANIMATION_V_SPEED = "vSpeed";
+    private static readonly string ANIMATION_EX_SPEED = "exSpeed";
+    private static readonly string ANIMATION_JUMP = "jump";
+    private static readonly string ANIMATION_GROUNDED = "grounded";
+    private static readonly string ANIMATION_DASHING = "dashing";
+    private static readonly string ANIMATION_WALL = "onWall";
+    private static readonly string ANIMATION_FACING = "facingRight";
+    private static readonly string ANIMATION_LADDER = "onLadder";
+    private static readonly string ANIMATION_INVULNERABLE = "invulnerable";
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
     }
 
     // Update is called once per frame
@@ -39,28 +49,31 @@ public class Move : SwitchCharachters
             rb.gravityScale = 5;
             moveSpeed = 10f;
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+            jumpForce = 50f;
         }
         else if (avatar2.activeSelf)
         {
             rb.gravityScale = 5;
-            moveSpeed = 20f;
+            moveSpeed = 50f;
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+            jumpForce = 0f;
         }
         else
         {
             rb.gravityScale = 2;
-            moveSpeed = 10f;
+            moveSpeed = 2f;
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+            jumpForce = 100f;
         }
     }
 
-    public void DoJump()
+    private void DoJump()
     {
-        
-        
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
         }
     }
 
 }
+
+
