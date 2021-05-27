e28754d984d5f573b013e12e78125f9381283e36
Nekozilla <krisagriza@gmail.com>
2021-05-27 13:19:14 +0300

    Finished ticket 14 -> created menu with option to resume, exit to main menu and to quit game. Pause is activated with escape key, to be activated with pause button on canvas.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void GoToMenu()
+    {
+        Debug.Log("Entering game menu...");
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
