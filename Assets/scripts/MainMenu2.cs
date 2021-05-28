3e67335389da1c46200b13f01fad9f74c7b0287e
Nekozilla <krisagriza@gmail.com>
2021-05-28 13:34:42 +0300

    now has level select screen

--- Assets/scripts/MainMenu2.cs
+++ Assets/scripts/MainMenu2.cs
@@ -7,7 +7,7 @@ public class MainMenu2 : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level1");
     }
 
     public void ExitGame()
